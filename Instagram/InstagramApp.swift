a0da81d0a644cec07de2ff822180faf5ada1a3ff
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-08-14 16:33:53 +0300

    init commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Ilya Schevchenko on 14.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
