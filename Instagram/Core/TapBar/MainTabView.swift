b57ae4b6f88faf7e7f519d3271fdab86c92c4f9a
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-08-14 16:55:28 +0300

    add tap bar for app

---
+++ Instagram/Core/TapBar/MainTabView.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabView.swift
+//  Instagram
+//
+//  Created by Ilya Schevchenko on 14.08.2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
