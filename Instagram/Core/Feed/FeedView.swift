eee4a9b1e0cafde8b9297e7db9c9020ae4c8c0a1
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-08-15 11:51:09 +0300

    create feed view with feed cell

--- Instagram/Core/Feed/FeedView.swift
+++ Instagram/Core/Feed/FeedView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram-logo")
+                        .resizable()
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 32)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "paperplane")
+                            .foregroundStyle(.black)
+                            .imageScale(.large)
+                    }
+                }
+            }
+        }
     }
 }
 
