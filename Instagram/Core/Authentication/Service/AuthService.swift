6428376ae5b0d8ca83922cc8f57f071a00937490
Ilya Shevchenko <95874401+InkaShev7777@users.noreply.github.com>
2024-08-19 12:37:52 +0300

    add uploading users in firebase database

--- Instagram/Core/Authentication/Service/AuthService.swift
+++ Instagram/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
+import Firebase
 
 class AuthService {
 
@@ -33,8 +35,10 @@ class AuthService {
     func createUser(email: String, password: String, username: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
-            
             self.userSession = result.user
+            print("DEBUG: Did create user...")
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
+            print("DEBUG: Did upload user data...")
         } catch {
             print("DEBUG: Faild registration user with error \(error.localizedDescription)")
         }
@@ -47,5 +51,12 @@ class AuthService {
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
 }
